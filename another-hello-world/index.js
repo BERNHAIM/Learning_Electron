772c76bf384a1b948f8d7f7338bdc005ca473cd4
BERNHAIM <giyami0425@gmail.com>
2020-10-04 12:26:29 +0900

    :beginner: electron build and package

---
+++ another-hello-world/index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function onClosed() {
+    mainWindow = null;
+}
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 640,
+        height: 480,
+
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+        }
+    });
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.on('closed', onClosed);
+}
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if(!mainWindow){
+        createWindow();
+    }
+});
+
+app.on('ready', () => {
+    createWindow();
+})
